d65c25e70401b3e8b9f71d67bbd3dbaae6a05690
noelphylan <noel_phylan@hotmail.com>
2018-08-28 21:30:04 +0100

    Add files via upload

--- BookingFilters/src/test/java/ui/constants/BookingConstants.java
+++ BookingFilters/src/test/java/ui/constants/BookingConstants.java
@@ -22,15 +22,16 @@ public class BookingConstants {
 	public static final String DESTINATION_INPUT_ID = "ss";	
 	public static final String XPATH_SEARCH_BUTTON = ".//button[@data-sb-id='main']";	
 	public static final String XPATH_CHECK_IN_DAY_MONTH_BUTTON =".//*[@id='frm']/div[1]/div[2]/div/div[2]/div/div/div/div[1]/div/button";
-	public static final String XPATH_CAL_MONTH_YEAR = "//th[@class='c2-month-header-monthname']";
-	public static final String XPATH_CAL_NAV_RIGHT = "//span[@class='c2-button-inner']";
+	public static final String XPATH_CAL_MONTH_YEAR = ".//th[@class='c2-month-header-monthname']";
+	public static final String XPATH_CAL_NAV_RIGHT = ".//span[@class='c2-button-inner']";
 	public static final String NAV_RIGHT_SYMBOL = "→";
+	public static final String NAV_LEFT_SYMBOL = "←";
 	public static final String XPATH_CAL_DAY = ".//span[@class='c2-day-inner']";
 	public static final String XPATH_5_STAR_FILTER_LINK = ".//a[@data-id='class-5']";
 	public static final String XPATH_SAUNA_FILTER_LINK = ".//a[@data-id='popular_activities-10']";
 	public static final String XPATH_HOTEL_FILTER_SPANS = ".//span[@class[contains(.,'sr-hotel__name')]]";
 	public static final String XPATH_REMOVE_FILTER_LINK = ".//ul[@class='option_remove_filters']/li/a";
-	public static final String XPATH_SURVEY_NO_BUTTON = ".//button[@class='survey_no_button']";	
+	public static final String XPATH_SURVEY_NO_BUTTON = ".//button[@class='survey_no_button']";
 	
 	
 	
